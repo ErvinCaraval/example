9644c946ecc36e0011bf4b92da6e9f3f9199ac32
ErvinCaraval <ervin.caravali@correounivalle.edu.co>
2024-05-30 02:12:11 -0500

    ddddpssddddttwwtwffttt

--- drf/settings.py
+++ drf/settings.py
@@ -99,13 +99,12 @@ WSGI_APPLICATION = 'drf.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': os.environ.get('DB_PORT'),
+        'NAME': 'projecto',
+        'USER': 'postgres',
+        'PASSWORD': 'Ec94',
+        'HOST': 'db',
+        'PORT': '90',  # Puedes dejarlo vacío si estás usando el puerto por defecto
     },
-  
 }
 
 
