5a99d891c8dc733519628a05f165607aef0aef53
ErvinCaraval <ervin.caravali@correounivalle.edu.co>
2024-05-31 13:39:54 -0500

    entregado

--- drf/settings.py
+++ drf/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
-# Build paths inside the project like this: BASE_DIR / 'subdi'
+# Build paths inside the project like this: BASE_DIR / 'subd'
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
